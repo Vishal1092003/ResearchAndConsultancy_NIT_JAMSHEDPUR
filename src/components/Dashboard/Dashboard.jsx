d85172f030ed0006153bb1b60d0917d9a5f84a3b
Aayush kumar <aayushkumarhigh@gmail.com>
2023-10-23 17:22:12 +0530

    logo update

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import './style.css'
 const Dashboard = () => {
     return (<>
         <div className=' w-full h-full flex justify-center items-center '>
-            <div className=' w-[70%] md:w-[20%] logo flex justify-center items-center'>
+            <div className=' shadow-2xl shadow-gray-400 rounded-full w-[70%] md:w-[20%] logo flex justify-center items-center'>
                 <img src={logo} alt="" />
             </div>
         </div>
