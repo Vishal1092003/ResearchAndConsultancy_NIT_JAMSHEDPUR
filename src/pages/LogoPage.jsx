62fdce33b1394fec15ece9fdd1675934ae7035fe
Aayush kumar <aayushkumarhigh@gmail.com>
2023-11-11 12:55:42 +0530

    logo tab curved

--- src/pages/LogoPage.jsx
+++ src/pages/LogoPage.jsx
@@ -5,7 +5,7 @@ const LogoPage = () => {
     <div className='w-[100vw] bg-yellow-50/50 h-[100vh]'>
       <motion.div className='w-full h-full' initial={{ y: -100, rotate: 180, scale: 0 }}
         animate={{ y: 0, rotate: 0, scale: 1 }}
-        transition={{ type: "spring", stiffness: 260, damping: 100 }}
+        transition={{ type: "spring", stiffness: 260, damping: 50 }}
       >
         <Dashboard />
       </motion.div>
