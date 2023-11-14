9869850c48d88efd627bb3dec7b608e36d0760c3
Sonu-Hansda <sonukumarhansda61@gmail.com>
2023-11-14 19:20:36 +0530

    Made text curve in dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -7,30 +7,43 @@ const Dashboard = () => {
         <div className='  w-full h-full flex justify-center items-center '>
             <div className="wrap">
                 <Link className=' logoTab flex justify-center group items-center ' to={'/home'}>
-                    <ReactCurvedText width={300}
-                        height={300}
-                        cx='255'
-                        cy='255'
-                        rx='145'
-                        ry='150'
-                        startOffset='37'
-                        reversed={true}
-                        tspanProps={{ "dy": "-30" }}
-                        text='Office Dean'
-                        textProps={{ "style": { "fontSize": "30" } }} />
+                    <ReactCurvedText
+                    width={300}
+                    height={300}    
+                    text='Office Dean'
+                    cx={250}
+                    cy={250}
+                    rx={180}
+                    ry={170}
+                    reversed={true}
+                    startOffset={30}
+                    textProps={{"style":{ "fontSize":"2rem"}}}
+                        // width={300}
+                        // height={300}
+                        // cx='255'
+                        // cy='255'
+                        // rx='145'
+                        // ry='150'
+                        // startOffset='37'
+                        // reversed={true}
+                        // tspanProps={{ "dy": "-30" }}
+                        // text='Office Dean'
+                        // textProps={{ "style": { "fontSize": 28 } }}
+                         />
                 </Link>
                 <Link className='  logoTab bg-blue-300 group  flex justify-center items-center' to='/home'>
-                    <ReactCurvedText width={300}
+                    <ReactCurvedText 
+                        width={300}
                         height={300}
-                        cx='300'
+                        cx='280'
                         cy='300'
-                        rx='210'
-                        ry='210'
-                        startOffset='35'
+                        rx='190'
+                        ry='200'
+                        startOffset='30'
                         reversed={true}
                         text='Research Expertise'
                         tspanProps={{ "dy": "-30" }}
-                        textProps={{ "style": { "fontSize": "30" } }}
+                        textProps={{ "style": { "fontSize": 28 } }}
                     />
                 </Link>
                 <Link className=' logoTab flex group justify-center items-center' to='/home'>
@@ -38,9 +51,9 @@ const Dashboard = () => {
                         height={300}
                         cx={40}
                         cy={260}
-                        rx={180}
-                        ry={185}
-                        startOffset='310'
+                        rx={195}
+                        ry={195}
+                        startOffset='350'
                         reversed={true}
                         text='Area of Research'
                         textProps={{ "style": { "fontSize": 30 } }}
@@ -54,8 +67,8 @@ const Dashboard = () => {
                         height={300}
                         cx={280}
                         cy={20}
-                        rx={200}
-                        ry={200}
+                        rx={205}
+                        ry={215}
                         startOffset='20'
                         reversed={false}
                         text='Sponsored Research'
@@ -68,11 +81,11 @@ const Dashboard = () => {
                 <Link className=' logoTab group flex justify-center items-center' to='/home'>
                     <ReactCurvedText width={300}
                         height={300}
-                        cx='20'
+                        cx='15'
                         cy='30'
-                        rx='220'
-                        ry='220'
-                        startOffset='380'
+                        rx='225'
+                        ry='230'
+                        startOffset='378'
                         reversed={false}
                         text='Research Consultancy'
                         textProps={{ "style": { "fontSize": 30 } }}
@@ -85,10 +98,10 @@ const Dashboard = () => {
                     <ReactCurvedText width={300}
                         height={300}
                         cx={290}
-                        cy={10}
-                        rx={200}
-                        ry={200}
-                        startOffset='70'
+                        cy={-10}
+                        rx={180}
+                        ry={255}
+                        startOffset='110'
                         reversed={false}
                         text='Collaboration'
                         textProps={{ "style": { "fontSize": 30 } }}
@@ -100,11 +113,11 @@ const Dashboard = () => {
                 <Link to='/home' className=' logoTab group bg-blue-300' >
                     <ReactCurvedText width={300}
                         height={300}
-                        cx='10'
-                        cy='10'
-                        rx='220'
+                        cx='35'
+                        cy='60'
+                        rx='230'
                         ry='220'
-                        startOffset='410'
+                        startOffset='430'
                         reversed={false}
                         text='Patent(IPRCELL)'
                         textProps={{ "style": { "fontSize": 30 } }}
@@ -117,14 +130,14 @@ const Dashboard = () => {
                     <ReactCurvedText width={300}
                         height={300}
                         cx={10}
-                        cy={300}
-                        rx={200}
-                        ry={210}
-                        startOffset='360'
+                        cy={310}
+                        rx={220}
+                        ry={190}
+                        startOffset='355'
                         reversed={true}
                         text='Research Facilities'
-                        textProps={{ "style": { "fontSize": 30 } }}
-                        tspanProps={{ "dy": "-30" }}
+                        textProps={{ "style": { "fontSize": 28 } }}
+                        tspanProps={{ "dy": "-30","dx":"30" }}
                         textPathProps={null} />
                 </Link>
                 <Link to='/home' className='logoTab'>
