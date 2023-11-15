9c9ff68b893cd525845b5b0d9494c7d3f524f509
Sonu-Hansda <sonukumarhansda61@gmail.com>
2023-11-15 15:54:00 +0530

    Created footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,40 @@
-
+import { Link } from "react-router-dom";
+import "../Footer/style.css";
+import { FaFacebook, FaYoutube, FaInstagram, FaTwitter, FaLinkedinIn } from "react-icons/fa6";
 const Footer = () => {
     return (
-        <div className='h-20 bg-sky-800'>
-            <div className='flex justify-center items-center h-full text-xl font-bold text-white'>
-                Footer
+        <div id="footer">
+            <div>
+                <div>
+                    <b>National Institute of<br />Technology Jamshedpur</b><br />
+                    Q4GV+RJ5, Adityapur, Jamshedpur, Jharkhand 831014
+                </div>
+                <div>
+                    <a href="tel:+"><i class="fa-solid fa-phone"></i> xxx xxx xxxx<br /></a>
+                    <a href="mailto:"><i class="fa-solid fa-envelope"></i> example@gmail.com</a>
+
+                </div>
+                <div className="flex">
+                    <Link to={"https://www.youtube.com/channel/UCSKf3jRXKuA9QkPsu69tzkQ"}>
+                    <FaYoutube className="mr-4 text-2xl" />
+                    </Link>
+                    <Link to={"https://www.instagram.com/nitjamshedpur_official/"}>
+                    <FaInstagram className="mr-4 text-2xl" />
+                    </Link>
+                    <Link to={"https://twitter.com/jamshedpur_nit?s=08"}>
+                    <FaTwitter className="mr-4 text-2xl" />
+                    </Link>
+                    <Link to={"https://www.facebook.com/NITJamshedpurOfficial/"}>
+                    <FaFacebook className="mr-4 text-2xl" />
+                    </Link>
+                    <Link to={"https://twitter.com/jamshedpur_nit?s=08"}>
+                    <FaLinkedinIn className="mr-4 text-2xl" />
+                    </Link>
+                </div>
             </div>
+            <div>s</div>
         </div>
+
     )
 }
 
