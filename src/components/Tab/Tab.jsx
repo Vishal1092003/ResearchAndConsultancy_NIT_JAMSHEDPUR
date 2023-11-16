1acc7b5b6b813e2ffac73464bb798c26b058fac2
Aayush kumar <aayushkumarhigh@gmail.com>
2023-11-16 18:35:13 +0530

    1.Added photos 2.Changes in tab 3.Changes in message section

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -3,11 +3,11 @@ import { } from 'react-icons/ri'
 const Tab = () => {
     return (
         <div className='flex justify-between h-10 px-5 py-6  items-center'>
-            <Link className=' transition  duration-200  py-1 px-2 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold ' >
-               Office Dean
+            <Link to={'/home'} className=' transition  duration-200  py-1 px-2 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold ' >
+                Home
             </Link>
-            <Link className=' transition  duration-200 py-1 px-1 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold '>
-                Area of Research  
+            <Link className=' transition  duration-200 py-1 px-1 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold ' to={'/Profile'}>
+                Expertise
             </Link>
             <Link className=' transition  duration-200 py-1 px-1 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold '>
                 Sponsored Research
@@ -15,20 +15,17 @@ const Tab = () => {
             <Link className=' transition  duration-200 py-1 px-1 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold '>
              Consultancy
             </Link>
-            <Link className=' transition  duration-200 py-1 px-1 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold 'to={'/Profile'}>
-             Expertise
-            </Link>
             <Link className=' transition  duration-200 py-1 px-1 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold '>
              Facilities
             </Link>
             <Link className=' transition  duration-200 py-1 px-1 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold '>
                 Patent(IPRCELL)
             </Link>
-            <Link className=' transition  duration-200 py-1 px-1 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold '>
+            <Link to={'/abc'} className=' transition  duration-200 py-1 px-1 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold '>
                 Collaboration
             </Link>
             <Link className=' transition  duration-200 py-1 px-1 hover:text-black/80  hover:bg-slate-200 rounded-md cursor-pointer text-slate-700/80 font-semibold '>
-               Interdisciplinary Action 
+                Interdisciplinary
             </Link>
            
         </div>
