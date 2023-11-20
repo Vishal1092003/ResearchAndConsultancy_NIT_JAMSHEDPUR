98d10840bd5d81d693aa1f6d83815d7535600ac7
Aayush kumar <aayushkumarhigh@gmail.com>
2023-11-20 15:54:31 +0530

    SponsoredResarchPageCompleted

--- src/pages/SponsoredResearch.jsx
+++ src/pages/SponsoredResearch.jsx
@@ -1,6 +1,19 @@
+import Navbar from '../components/Navbar/Navbar'
+import SponseredResearchData from '../Data/SponseredResearchData.json'
+import SponsoredResearchDataComponent from '../components/SponsoredResearchDataComponent/SponsoredResearchDataComponent'
 const SponsoredResearch = () => {
     return (
-        <div>SponsoredResearch</div>
+        <div className='w-[100vw] h-[100vh]'>
+            <Navbar />
+            <div className='w-[98%] m-auto h-20 mt-5 rounded-lg shadow-lg flex justify-center items-center bg-sky-200'>
+                <div className=' font-semibold text-xl'>
+                    Sponsored Research
+                </div>
+            </div>
+            <div className=' pb-10'>
+                {SponseredResearchData.Sheet.map((item, index) => (<SponsoredResearchDataComponent title={item.TITLE} principalInvestigator={item.PRINCIPALINVESTIGATOR} fundingAgency={item.FUNDINGAGENCY} amount={item.AMOUNT} index={index} key={index} />))}
+            </div>
+        </div>
     )
 }
 
