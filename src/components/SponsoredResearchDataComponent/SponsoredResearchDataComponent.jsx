dfb21396638d3fd49628ff21212b004370f8efbb
Aayush kumar <aayushkumarhigh@gmail.com>
2023-11-21 18:46:48 +0530

    responsive changes in sponsored Research page

--- src/components/SponsoredResearchDataComponent/SponsoredResearchDataComponent.jsx
+++ src/components/SponsoredResearchDataComponent/SponsoredResearchDataComponent.jsx
@@ -6,11 +6,11 @@ const SponsoredResearchDataComponent = ({ index, title, principalInvestigator, f
     return (<>
         <div onClick={() => { setIsOpen(!isOpen) }} className='w-[98%] m-auto h-16 mt-4 rounded-t-lg shadow-lg flex justify-center items-center bg-amber-50'>
             <div className='flex justify-between mx-10 items-center w-full  '>
-                <div className=' font-semibold text-xl flex justify-center items-center'>
+                <div className=' font-semibold md:text-xl text-lg flex justify-center items-center'>
                     <div className='mx-3'>
                         {index + 1}.
                     </div>
-                    <div className=''>
+                    <div className=' overflow-hidden flex md:items-center items-start leading-tight  h-[50px]'>
                         {title}
                     </div>
                 </div>
@@ -19,21 +19,31 @@ const SponsoredResearchDataComponent = ({ index, title, principalInvestigator, f
                 </motion.div>
             </div>
         </div>
-        <div className={`${isOpen ? 'h-[15vh] ' : 'h-0 '} shadow-lg rounded-b-lg w-[98%] m-auto  flex-col  transition-all duration-300  bg-amber-100`}>
+        <div className={`${isOpen ? 'md:h-[15vh] h-[10vh] ' : 'h-0 '} shadow-lg rounded-b-lg w-[98%] m-auto  flex-col  transition-all duration-300  bg-amber-100`}>
             <div className={`${isOpen ? 'flex ' : 'hidden '} font-semibold text-xl justify-around items-center h-full w-full`}>
                 <div className=' flex flex-col md:text-xl text-sm md:flex-row'>
                     <div className='mx-3 '>
                         Principal Investigator:
                     </div>
-                    <div>
+                    <div className=' text-center font-medium'>
                         {principalInvestigator}
                     </div>
                 </div>
-                <div className=''>
-                    Funding Agency: {fundingAgency}
+                <div className='flex flex-col md:text-xl text-sm md:flex-row'>
+                    <div className='mx-3'>
+                        Funding Agency:
+                    </div>
+                    <div className=' text-center font-medium'>
+                        {fundingAgency}
+                    </div>
                 </div>
-                <div className=''>
-                    Amount: {amount}
+                <div className='flex flex-col md:text-xl text-sm md:flex-row'>
+                    <div className='mx-3'>
+                        Amount:
+                    </div>
+                    <div className=' text-center font-medium'>
+                        {amount}
+                    </div>
                 </div>
             </div>
         </div>
