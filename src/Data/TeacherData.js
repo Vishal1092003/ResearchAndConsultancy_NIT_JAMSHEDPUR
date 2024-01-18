e6a6f8dd3cd4dfebdcafce5a395c4c90bb676741
Aayush kumar <aayushkumarhigh@gmail.com>
2024-01-18 17:10:42 +0530

    some change

---
+++ src/Data/TeacherData.js
@@ -0,0 +1,30 @@
+import AshokSir from "../assets/AshokSir.jpg";
+import DrKanika from "../assets/Dr.Kanika.jpeg";
+import ArunPandit from "../assets/ArunPandit.png";
+const data = {
+  DataSheet: [
+    {
+      id: 1,
+      NAME: "Ashok Kumar Mandal",
+      PHOTO: AshokSir,
+      DESIGNATION: "Assistant Professor",
+      DEPARTMENT: "Mechanical Engineering",
+    },
+    {
+      id: 2,
+      NAME: "Dr.Kanika",
+      PHOTO: DrKanika,
+      DESIGNATION: "Assistant Professor",
+      DEPARTMENT: "Computer Science and Engineering",
+    },
+    {
+      id: 3,
+      NAME: "Bhupali Dutta",
+      PHOTO: ArunPandit,
+      DESIGNATION: "Assistant Professor",
+      DEPARTMENT: "Civil Engineering",
+    },
+  ],
+};
+
+export default data;
--- src/pages/Expretise.jsx
+++ src/pages/Expretise.jsx
@@ -5,39 +5,48 @@ import Sidebar from "../components/Sidebar/Sidebar"
 import Profile from "../components/Teacher/Profile"
 import { AiFillProfile } from "react-icons/ai"
 import ReserchAreaData from "../Data/ReserchAreaData.json"
-import TeacherData from "../Data/TeacherData.json"
+import TeacherData from "../Data/TeacherData.js";
 const Expretise = () => {
-    return (
-        <>
-            <div className="w-[100vw] h-[100vh]">
-                <Navbar />
-                <div className="flex bg-yellow-50/50">
-                    <div className="w-[20%] m-3">
-                        <div className="w-full text-center p-3  font-medium text-xl rounded-xl bg-sky-300">
-                            Research Area
-                        </div>
-                        <div className="p-4 ">
-                            {
-                                ReserchAreaData.resarchArea.map((item, index) => (<Sidebar Icon={AiFillProfile} ResarchArea={item.RA} key={index} />))
-                            }</div>
-                    </div>
-                    <div className=" border-l-2 border-sky-100 m-2">
-
-                    </div>
-                    <div className="w-[80%] m-3">
-                        <div className="w-full text-center p-3 font-semibold text-2xl rounded-xl bg-sky-300 ">
-                            Professors
-                        </div>
-                        <div className="flex mt-5 gap-20 flex-wrap ">
-                            {
-                                TeacherData.DataSheet.map((item, id) => (<Card  name={item.NAME} photo={item.PHOTO} designation={item.DESIGNATION} department={item.DEPARTMENT} key={id}/>))
-                            }
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="w-[100vw] h-[100vh]">
+        <Navbar />
+        <div className="flex bg-yellow-50/50">
+          <div className="w-[20%] m-3">
+            <div className="w-full text-center p-3  font-medium text-xl rounded-xl bg-sky-300">
+              Research Area
             </div>
-        </>
-    )
-}
+            <div className="p-4 ">
+              {ReserchAreaData.resarchArea.map((item, index) => (
+                <Sidebar
+                  Icon={AiFillProfile}
+                  ResarchArea={item.RA}
+                  key={index}
+                />
+              ))}
+            </div>
+          </div>
+          <div className=" border-l-2 border-sky-100 m-2"></div>
+          <div className="w-[80%] m-3">
+            <div className="w-full text-center p-3 font-semibold text-2xl rounded-xl bg-sky-300 ">
+              Professors
+            </div>
+            <div className="flex mt-5 gap-20 flex-wrap ">
+              {TeacherData.DataSheet.map((item, id) => (
+                <Card
+                  name={item.NAME}
+                  photo={item.PHOTO}
+                  designation={item.DESIGNATION}
+                  department={item.DEPARTMENT}
+                  key={id}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Expretise
