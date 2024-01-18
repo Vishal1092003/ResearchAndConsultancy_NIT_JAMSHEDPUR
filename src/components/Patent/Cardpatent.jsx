5e743d7b661b7c2490280bf2c66427689e443c75
Rahul62066 <127167830+Rahul62066@users.noreply.github.com>
2024-01-18 18:39:15 +0530

    Card hover

--- src/components/Patent/Cardpatent.jsx
+++ src/components/Patent/Cardpatent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent} from '../ui/card.jsx'
 const Cardpatent = () => {
   return (<>
-   <Card className='w-60 h-72'>
+   <Card className='w-60 h-72 hover:shadow-lg hover:bg-emerald-100 bg-emerald-200'>
             <CardHeader>
                 <div>
                     <CardTitle>Title</CardTitle>
