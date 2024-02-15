fffbba0a57f45d5085451c1605f116bb9c609105
Aayush kumar <aayushkumarhigh@gmail.com>
2024-02-15 19:28:46 +0530

    message fixed

---
+++ src/components/DeanMessage/DeanMessage.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { DeanSir } from "@/assets";
+const DeanMessage = () => {
+  return (
+    <motion.div
+      initial={{ y: 50, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.7 }}
+    >
+      <div className="w-full flex p-4 min-h-[70vh] justify-center ">
+        <div className="flex items-center justify-center flex-col-reverse md:flex-row shadow-lg bg-sky-100 rounded-md md:w-[90%] w-[92vw]">
+          <div className="mx-5">
+            <div className="py-5 text-md font-serif font-medium px-4 h-[1400px] md:h-[650px] flex justify-center text-center md:mt-10 mb-10 md:text-left  items-center ">
+              <div className=" tracking-wide">
+                I am delighted to emphasize our institute's strong dedication to
+                fostering a culture of research and innovation. With an
+                unwavering commitment deeply rooted in expanding the boundaries
+                of knowledge, we actively encourage and support research-driven
+                initiatives across various departments. Our specialized
+                departments are centres where advanced research takes place,
+                covering a wide range of subjects. <br /> In addition, our
+                dedication to innovation goes beyond theory. We take pride in
+                turning research outcomes into real-world solutions and actively
+                seek patents for unique ideas. This not only adds value to our
+                researchers' work but also emphasizes our commitment to
+                advancing society. Our institute is fundamentally a place of
+                great importance in terms of research and innovation.
+                <br /> Moreover, we have Memoranda of Understanding (MoUs)
+                signed with different academic institutions and industries to
+                jointly work on different projects. The learned faculty members
+                and promising students of our institute along with persons with
+                good industrial experience exchange their expertise for mutual
+                benefit and growth.These initiatives not only address current
+                challenges but also drive our mutual journey for knowledge.{" "}
+                <br />
+                At the core of our beliefs lies a deep commitment to protecting
+                intellectual property rights (IPR). We fully understand the
+                importance of safeguarding the creative and ground-breaking
+                ideas that emerge from within our academic community.Our
+                institute is dedicated to assisting researchers in navigating
+                the complex realm of IPR, ensuring that their valuable work
+                remains shielded and available for the greater good. <br />
+                We are determined to create an environment that fosters
+                ground-breaking ideas, safeguards intellectual property, and
+                transforms research into practical applications. Working
+                together, we embark on an ongoing journey of exploration and
+                advancement.
+                <br />
+                Best regardss
+              </div>
+            </div>
+          </div>
+          <div className="h-full flex justify-center items-center flex-col min-w-[22rem] md:min-w-[25rem] min-h-[5rem]  py-4 rounded-md">
+            <img
+              className=" rounded-xl max-w-[15rem] md:max-w-[20rem]"
+              src={DeanSir}
+              width={1000}
+              height={1000}
+              alt=""
+            />
+            <div className="w-full flex-col text-center flex py-2 justify-center font-semibold text-lg">
+              <div>{"Prof.MK Sinha"}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default DeanMessage;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import Message from '../components/Message/Message'
 import Navbar from '../components/Navbar/Navbar'
 import Notice from '../components/Notice/Notice'
 import { DirectorSir, DeanSir, DrKanika, OmSir, AshokSir, Subhash, RahulMahato, ArunPandit, } from '../assets/index'
+import DirectorMessage from "@/components/DirectorMessage/DirectorMessage";
+import DeanMessage from "@/components/DeanMessage/DeanMessage";
 const Home = () => {
   return (
     <>
@@ -14,11 +16,6 @@ const Home = () => {
           <div className="pt-10 flex gap-5 flex-wrap justify-center ">
             <HashLink to={"./#directormessage"}>
               <div className="w-96 h-14  flex justify-center items-center  rounded-lg  align-centre gap-5 bg-slate-200 text-2xl border-y-2 hover:shadow-lg transition  duration-200 py-1 px-1   hover: bg-blue-50/50 cursor-pointer">
-                <img
-                  className="w-50 h-12 rounded-md  "
-                  src={DirectorSir}
-                  alt="Direc photo"
-                />
                 <div> Director's Message</div>
               </div>
             </HashLink>
@@ -50,27 +47,8 @@ const Home = () => {
             <div className="flex bg-sky-200 w-full shadow-md p-2 mx-4 rounded-lg  font-medium text-3xl font-serif justify-center items-center">
               Director's Message
             </div>
-            <Message
-              img={DirectorSir}
-              name={"Prof. (Dr.) Goutam Sutradhar"}
-              message1={`As I reflect upon my tenure as the Director of NIT Jamshedpur, it brings me immense joy to witness and contribute to the remarkable achievements and accolades earned by the dedicated students, faculty members and staffs of NIT Jamshedpur, a testament to their unwavering hard work and commitment.
 
-The excellence of NIT Jamshedpur is vividly demonstrated through its steadfast dedication to cutting-edge research across a diverse spectrum. Our commitment to fostering an environment of academic excellence and innovation has been a guiding principle throughout the institution's history. In recent years, NIT Jamshedpur has taken significant strides towards integrating contemporary methods that infuse new technologies and skills into the curriculum. 
-
-Moreover, NIT Jamshedpur has actively pursued collaborations with other esteemed
-institutes, exemplified by the signing of Memoranda of Understanding (MoUs). These
-agreements expand our knowledge base, resources, and access to experts, enriching
-`}
-              message2={`the educational experience for our bright minds. The collaborations not only enhance
-research projects but also provide students and faculty members with the opportunities to engage in workshops, fostering a dynamic and collaborative learning environment.
-
-In addition, NIT Jamshedpur has a dedicated Intellectual Property Rights (IPR) Cell, reinforcing our commitment to the authorization and protection of research work. This initiative ensures that the innovative ideas generated within our institution are duly recognized and protected.
-
-As we continue to chart new territories in academia and research, I am optimistic about the future of NIT Jamshedpur. The journey ahead holds endless possibilities, and I am confident that our collective efforts will propel this institution to even greater heights of success and recognition.
-
-Best Wishes
-`}
-            />
+            <DirectorMessage />
           </div>
         </div>
         <div className="pt-5 px-4 bg-yellow-50/50">
@@ -83,19 +61,7 @@ Best Wishes
             </div>
             <div>
               <div>
-                <Message
-                  img={DeanSir}
-                  message1={`
-                    I am delighted to emphasize our institute's strong dedication to fostering a culture of research and innovation. With an unwavering commitment deeply rooted in expanding the boundaries of knowledge, we actively encourage and support research-driven initiatives across various departments. Our specialized departments are centres where advanced research takes place, covering a wide range of subjects. 
-In addition, our dedication to innovation goes beyond theory. We take pride in turning research outcomes into real-world solutions and actively seek patents for unique ideas. This not only adds value to our researchers' work but also emphasizes our commitment to advancing society. Our institute is fundamentally a place of great importance in terms of research and innovation. 
-Moreover, we have Memoranda of Understanding (MoUs) signed with different academic institutions and industries to jointly work on different projects. The learned faculty members and promising students of our institute along with persons with good industrial experience exchange their expertise for mutual benefit and growth.These initiatives not only address current challenges but also drive our mutual journey for knowledge.
-At the core of our beliefs lies a deep commitment to protecting intellectual property rights (IPR). We fully understand the importance of safeguarding the creative and ground-breaking ideas that emerge from within our academic community.Our institute is dedicated to assisting researchers in navigating the complex realm of IPR, ensuring that their valuable work remains shielded and available for the greater good. 
-We are determined to create an environment that fosters ground-breaking ideas, safeguards intellectual property, and transforms research into practical applications. Working together, we embark on an ongoing journey of exploration and advancement.
-Best regards
-`}
-                  message2=""
-                  name={"Prof.MK Sinha"}
-                />
+                <DeanMessage />
               </div>
             </div>
           </div>
@@ -168,6 +134,6 @@ Best regards
       </div>
     </>
   );
-}
+};
 
 export default Home
