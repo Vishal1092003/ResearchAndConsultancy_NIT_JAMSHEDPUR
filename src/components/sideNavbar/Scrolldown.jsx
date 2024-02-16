d53ece1265ade4ea0cf53c684ebe789dbbe5d9e1
Rahul62066 <127167830+Rahul62066@users.noreply.github.com>
2024-02-16 16:03:16 +0530

    icons

--- src/components/sideNavbar/Scrolldown.jsx
+++ src/components/sideNavbar/Scrolldown.jsx
@@ -6,25 +6,26 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion"
-
+import {FaIndustry,FaUniversity,FaHandshake} from 'react-icons/fa'
+import {BiSolidInstitution} from 'react-icons/bi'
 const Scrolldown = () => {
     return (
         <div><Accordion type="single" collapsible>
             <AccordionItem value="item-1">
-                <AccordionTrigger className="text-xl">Collaboration</AccordionTrigger>
+                <AccordionTrigger className="text-xl flex gap-1 justify-center items-center"><FaHandshake />Collaboration</AccordionTrigger>
                 <Link  to="/academia">
-                    <AccordionContent className='text-xl font-bold'>
-                        Academia
+                    <AccordionContent className='text-xl flex gap-1 justify-center items-center font-bold'>
+                       <FaUniversity /> Academia
                     </AccordionContent>
                 </Link>
                 <Link to="/industry">
-                    <AccordionContent className='text-xl font-bold'>
-                        Industry
+                    <AccordionContent className='text-xl flex gap-1 justify-center items-center font-bold'>
+                      <FaIndustry />  Industry
                     </AccordionContent>
                 </Link>
                 <Link to="/researchinstitute">
-                    <AccordionContent className='text-xl font-bold'>
-                        Research Institute
+                    <AccordionContent className='text-xl flex gap-1 justify-center items-center font-bold'>
+                      <BiSolidInstitution />  Research Institute
                     </AccordionContent>
                 </Link>
             </AccordionItem>
