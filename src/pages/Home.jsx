262672e5ffb61a7062e1d008be37494de3c3fe1a
vishal kumar <vishalsaw1092003@gmail.com>
2024-02-28 18:06:30 +0530

    r and c vishhal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,21 @@ import Footer from '../components/Footer/Footer'
 import Message from '../components/Message/Message'
 import Navbar from '../components/Navbar/Navbar'
 import Notice from '../components/Notice/Notice'
+
+import Marquee from "react-fast-marquee";
 import { DirectorSir, DeanSir, DrKanika, OmSir, AshokSir, Subhash, RahulMahato, ArunPandit, } from '../assets/index'
 import DirectorMessage from "@/components/DirectorMessage/DirectorMessage";
 import DeanMessage from "@/components/DeanMessage/DeanMessage";
+
 const Home = () => {
+
   return (
     <>
       <div className="h-[100vh] w-[100vw]  ">
         <Navbar />
         <div className=" z-10 flex bg-yellow-50/50  justify-center items-center flex-col ">
+
+          {/* director's message dean's message */}
           <div className="py-5 flex gap-5 flex-wrap justify-center ">
             <HashLink to={"./#directormessage"}>
               <div className="md:w-96 w-24 h-14  flex justify-center items-center  rounded-lg  align-centre gap-5 bg-slate-200 md:text-2xl text-md font-semibold md:font-medium border-y-2 hover:shadow-lg transition  duration-200 py-1 px-1   hover: bg-blue-50/50 cursor-pointer">
@@ -28,20 +34,57 @@ const Home = () => {
                 <div className="md:w-96 w-24 h-14   rounded-lg flex items-center justify-center align-centre bg-slate-200 gap-5  md:text-2xl text-md font-semibold md:font-medium border-y-2 hover:shadow-lg transition  duration-200 py-1 px-1   hover: bg-blue-50/50  cursor-pointer">
                   <div>
                     {" "}
-                    Dean <span className=" hidden md:inline">Message</span>{" "}
+                  Dean&apos;s{" "} <span className=" hidden md:inline">Message</span>{" "}
                   </div>
                 </div>
               </HashLink>
             </div>
-            <HashLink to={"./#Associatedeans"}>
-              <div className="md:w-96 w-28 h-14   rounded-lg flex items-center justify-center align-centre bg-slate-200 gap-5  md:text-2xl text-md font-semibold md:font-medium border-y-2 hover:shadow-lg transition  duration-200 py-1 px-1   hover: bg-blue-50/50  cursor-pointer">
-                <div className=" flex justify-center items-center">
-                  <span className=" hidden md:inline">Associate</span>
-                  <span className=" inline md:hidden">Assoc.</span> Deans
+<HashLink to={"./#Associatedeans"}>
+  <div className="md:w-96 w-24 h-14   rounded-lg flex items-center justify-center align-centre bg-slate-200 gap-5  md:text-2xl text-md font-semibold md:font-medium border-y-2 hover:shadow-lg transition  duration-200 py-1 px-1   hover: bg-blue-50/50  cursor-pointer">
+                  <div>
+                    {" "}
+                  Associate{" "} <span className=" hidden md:inline">Deans</span>{" "}
+                  </div>
                 </div>
-              </div>
-            </HashLink>
+</HashLink>
+
+
           </div>
+    <div className='flex'>
+      <div className='ml-5  '>
+         <h1 className='font-bold text-center'>Events: </h1>
+      </div>
+      <div className='ml-2'>
+        <Marquee>
+
+   <div className='ml-5'>
+       <p>
+      Upcoming: Workshop of Educational Outreach Program of Bureau of Indian Standards on 21/02/2024 
+            <a href='' className='text-blue-500'> view</a>
+    </p>
+    
+   </div>
+   
+    <div className='ml-5'>
+     <p>
+      Past-Events: Workshop of Educational Outreach Program of Bureau of Indian Standards on 21/02/2024 
+      <a href='' className='text-blue-500'> view</a>
+    </p>
+    </div>
+    
+  
+
+ </Marquee>
+ 
+      </div>
+    </div>
+ 
+  
+
+         
+        
+
+
           <div className="md:p-x-12 md:pb-12 p-0 md:flex-row flex-col gap-x-2 shadow-md flex rounded-lg">
             <div className="w-96 md:w-full flex justify-center items-center flex-col">
               <CarouselComponents />
@@ -67,8 +110,10 @@ const Home = () => {
               id="Deanmessage"
               className="flex bg-sky-200 shadow-md text-center p-2 mx-4 rounded-lg  font-medium text-3xl font-serif justify-center items-center"
             >
-              Dean Message
+              Dean's Message
+              
             </div>
+
             <div>
               <div>
                 <DeanMessage />
