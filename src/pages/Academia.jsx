8b529c5318b6be9fbdb1eb708f136ae93dc131d4
Ashu1109 <aayushkumarhigh@gmail.com>
2025-02-09 10:38:08 +0530

    Sponsored Research changes

--- src/pages/Academia.jsx
+++ src/pages/Academia.jsx
@@ -6,10 +6,6 @@ const Academia = () => {
     return (
         <div className="w-[100vw] h-[100vh] ">
             <Navbar />
-
-            
-                
-          
             <div className="p-5 flex justify-center flex-col align-centre ">
                 <div className='w-full  h-16 mt-5 rounded-lg shadow-lg flex justify-center items-center bg-sky-300'>
                     <div className=' font-bold md:text-2xl text-xl'>
